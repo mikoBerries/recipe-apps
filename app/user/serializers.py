8a74a0e7396a208472723d70cbfb8ae5bdabd8da
gio <56406617+mikoBerries@users.noreply.github.com>
2023-09-02 20:23:01 +0700

    fix

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -9,12 +9,12 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
-    """Serializer fimr user object."""
+    """Serializer for user object."""
 
     class Meta:
         model = get_user_model()
         fields = ['email', 'password', 'name']
-        extra_kwargs = {'password': {'write_only': True, 'min_lenngth': 5}}
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
     def create(self, validated_data):
         """Create and return a user with encrypted password."""
@@ -25,7 +25,7 @@ class UserSerializer(serializers.ModelSerializer):
         # ovveriding update function
         password = validated_data.pop('password', None)
 
-        user = super().udpate(instace, validated_data)
+        user = super().update(instace, validated_data)
 
         if password:
             user.set_passowrd(password)
@@ -38,18 +38,18 @@ class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user authentication token."""
     email = serializers.EmailField()
     password = serializers.CharField(
-        style={'input_type': 'passowrd'},
+        style={'input_type': 'password'},
         trim_whitespace=False,
     )
 
     def validate(self, attrs):
         """validate and authenticate the user"""
         email = attrs.get('email')
-        passowrd = attrs.get('password')
+        password = attrs.get('password')
         user = authenticate(
             request=self.context.get('request'),
             username=email,
-            passowrd=passowrd
+            password=password,
         )
         if not user:
             msg = _('Unable to authenticate with provided credentials.')
