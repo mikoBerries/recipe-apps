de07ba0d11f503a7485666833e5ea6caf4fa9564
gio <56406617+mikoBerries@users.noreply.github.com>
2023-09-03 19:52:45 +0700

    implementing nested tags in recipe models

--- app/recipe/serializer.py
+++ app/recipe/serializer.py
@@ -7,16 +7,40 @@ from rest_framework import serializers
 from core.models import Recipe, Tag
 
 
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tags."""
+    class Meta:
+        model = Tag
+        fields = '__all__'
+        read_only_fields = ['id']
+
+
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes."""
+    tags = TagSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
         # fields = "__all__"
-        read_only_fields = ['id', 'create_on', 'update_on']
         fields = ['id', 'title', 'time_munites',
-                  'price', 'link', 'create_on', 'update_on']
+                  'price', 'link', 'create_on', 'update_on', 'tags']
+        read_only_fields = ['id', 'create_on', 'update_on']
         # exclude = ['id']
+
+    def create(self, validate_data):
+        """Create a recipe"""
+        # Overriding create function for recipe
+        tags = validate_data.pop('tags', [])
+        recipe = Recipe.objects.create(**validate_data)
+        auth_user = self.context['request'].user
+        for tag in tags:
+            tag_obj, created = Tag.objects.get_or_create(
+                user=auth_user, **tag,
+            )
+            recipe.tags.add(tag_obj)
+
+        # recipe will created when recipe queryset used
+        return recipe
 
 
 class RecipeDetailSerializer(RecipeSerializer):
